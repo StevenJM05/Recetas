3af918f55383f2f70add6ddfda5514e2d33b4f2f
StevenJM05 <116135869+StevenJM05@users.noreply.github.com>
2024-11-18 18:44:09 -0600

    Crear RecipeController

--- Recetas/Controllers/RecipesController.cs
+++ Recetas/Controllers/RecipesController.cs
@@ -7,7 +7,7 @@ namespace Recetas.Controllers
 {
 	[Route("api/[controller]")]
 	[ApiController]
-	public class RecipesController :ControllerBase
+	public class RecipesController : ControllerBase
 	{
 		private readonly RecipeDBContext _context;
 
@@ -41,6 +41,58 @@ namespace Recetas.Controllers
 			}
 
 			return recipe;
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<Recipe>> PostRecipe(Recipe recipe)
+		{
+			_context.Recipes.Add(recipe);
+			await _context.SaveChangesAsync();
+
+			return CreatedAtAction(nameof(GetRecipe), new {id = recipe.Id}, recipe);
+		}
+
+		[HttpPut("{id}")]
+		public async Task<IActionResult> PutRecipe(int id, Recipe recipe)
+		{
+			if(id != recipe.Id)
+			{
+				return BadRequest();
+			}
+
+			_context.Entry(recipe).State = EntityState.Modified;
+
+			try
+			{
+				await _context.SaveChangesAsync();
+			}catch(DbUpdateConcurrencyException)
+			{
+				if(!_context.Recipes.Any(e => e.Id == id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
+
+			return NoContent();
+		}
+
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteRecipe(int id)
+		{
+			var recipe = await _context.Recipes.FindAsync(id);
+			if (recipe == null)
+			{
+				return NotFound();
+			}
+
+			_context.Recipes.Remove(recipe);
+			await _context.SaveChangesAsync();
+
+			return NoContent();
 		}
 
 	}
